50d88ec3003a2df718366ec2fd5ab42eaefdec54
Piyush75812 <piyushkumar63072@gmail.com>
2025-09-22 23:49:40 +0530

    Add files via upload

--- RemoveDuplicatesSorted.java
+++ RemoveDuplicatesSorted.java
@@ -1,16 +1,16 @@
 class RemoveDuplicatesSorted{
     public static void main(String args[]){
-        int nums[] = {0,0,1,1,1,2,2,3,4};
+        int nums[] = {1,1,2,2,3};
         System.out.println(removeDuplicates(nums));
     }
-    public static int removeDuplicates(int[] nums) {
-        int count = 0;
+    public static int removeDuplicates(int nums[]){
+        int count=0;
         for(int i=0;i<nums.length;i++){
-            if(i<nums.length-1 && nums[i]==nums[i+1]){
-                continue;
+            if(i<nums.length-1 && nums[i] == nums[i+1]){
+                 continue;
             }
             else{
-                nums[count] = nums[i];
+                nums[count] =nums[i];
                 count++;
             }
         }
