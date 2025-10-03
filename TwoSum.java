ddd0f29f6b88c61d11dfa675fea7f5d49b813735
Piyush75812 <piyushkumar63072@gmail.com>
2025-10-03 18:46:49 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class TwoSum {
+    public static void main(String args[]) {
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result)); 
+    }
+
+    public static int[] twoSum(int nums[], int target) {
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int rem = target - num;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == rem) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; 
+    }
+}
