c39600628cbec3819c448558db671a3780569160
Piyush75812 <piyushkumar63072@gmail.com>
2025-10-06 20:13:57 +0530

    Add files via upload

---
+++ ClimbingStairs.java
@@ -0,0 +1,17 @@
+class ClimbingStairs{
+    public static void main(String args[]){
+        System.out.println(climbingStairs(5));
+    }
+    public static int climbingStairs(int n){
+        if(n<=3){
+            return n;
+        }
+        int a=3;
+        int b=2;
+        for(int i=0;i<n-3;i++){
+               a=a+b;
+               b=a-b;
+        }
+        return a;
+    }
+}
